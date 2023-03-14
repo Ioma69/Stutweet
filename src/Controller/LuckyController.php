febef9466a7822ffc18882d50fa919bd2537a93a
Sariel RAQUIN <sarielr@hotmail.fr>
2023-03-14 12:22:19 +0100

    init

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("lucky")             // AU LIEU d'utulise routes.yaml
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response("<html><h1>Votre nombre fétiche : $number</h1></html>");
+    }
+
+      #[Route("random")]                                    // ATTRIBUTS PHP 8                                          
+     
+    public function random(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render("/lucky/number.html.twig");
+    }
+}
+
+
+
+
