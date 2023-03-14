f3df0fe4ce80ffdc8be85a09b16f56a47d978075
Sariel RAQUIN <sarielr@hotmail.fr>
2023-03-14 16:40:19 +0100

    twig

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -21,10 +21,11 @@ class LuckyController extends AbstractController
       #[Route("random")]                                    // ATTRIBUTS PHP 8                                          
      
     public function random(): Response
+    
     {
-        $number = random_int(0, 100);
-
-        return $this->render("/lucky/number.html.twig");
+        return $this->render("/lucky/number.html.twig",[
+            "number" => random_int(0, 100)
+        ]);
     }
 }
 
